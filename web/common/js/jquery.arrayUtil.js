5f17532220968e5a1eb61ea6c821d1f41e219194
Jongho,Lee <jongholee73@gmail.com>
2013-01-25 11:03:43 +0900

    jquery.arrayUtil.js (Array 함수)

---
+++ web/common/js/jquery.arrayUtil.js
@@ -0,0 +1,94 @@
+/*===============================================================================
+ * 최대값 구하는 함수
+ *==============================================================================*/
+Array.prototype.max = function () {
+    return Math.max.apply(null,this);
+}
+
+/*===============================================================================
+ * 최소값 구하는 함수
+ *==============================================================================*/
+Array.prototype.min = function () {
+    return Math.min.apply(null,this);
+}
+
+/*===============================================================================
+ * 평균 구하는 함수
+ *==============================================================================*/
+Array.prototype.average = function () {
+    if(!Object.prototype.toString.call(this) === "[object Array]") { return false;	}
+    var i = this.length,
+        sum = 0;
+    while( i-- ){
+        sum += this[i];
+    }
+    return (sum / this.length );
+}
+
+/*===============================================================================
+ * 분산 구하는 함수
+ *==============================================================================*/
+Array.prototype.variance = function () {
+    if(!Object.prototype.toString.call(this) === "[object Array]") { return false; }
+    var avg = this.average()
+        , i = this.length
+        , v = 0;
+
+    while( i-- ){
+        v += Math.pow((this[i] - avg),2);  //편차(각변량-평균)의 제곱의 합
+    }
+    v /= this.length;  //분산 = 편차의 제곱의 합 / 총 변량의 개수
+    return v;
+}
+
+/*===============================================================================
+ * 표준편차 구하는 함수
+ *==============================================================================*/
+Array.prototype.stdDev = function () {
+    if(!Object.prototype.toString.call(this) === "[object Array]") { return false; }
+    var stdDev = Math.sqrt(this.variance());  //표준편차 = 분산의 제곱근
+    return stdDev;
+}
+
+/*===============================================================================
+ * HISTOGRAM 그리는 함수
+ *==============================================================================*/
+Array.prototype.resultHistogramList = function (callback){
+
+    var minVal = Math.min.apply(null,this);
+    var maxVal = Math.max.apply(null,this);
+
+    var rVal        = [0,0,0,0,0,0,0,0,0,0];
+    var compVal     = [0,0,0,0,0,0,0,0,0,0];
+    var categoryVal = [0,0,0,0,0,0,0,0,0,0];
+    var cdf         = [0,0,0,0,0,0,0,0,0,0];
+
+    var mVal = (maxVal - minVal) / 10;
+
+    /*categoryVal 구하기*/
+    for(i = 0; i<categoryVal.length; i++){
+        compVal[i] = minVal + mVal*(i);
+        categoryVal[i] = ( compVal[i] + mVal/2 ).toFixed(2);
+    }
+
+    /*rVal 구하기*/
+    for(i =0; i< this.length; i++){
+        for(j=compVal.length-1; j>=0; j--){
+            if( this[i] >= compVal[j]){
+                rVal[j] = rVal[j] + 1;
+                break;
+            }
+        }
+    }
+
+    /*cdf 구하기*/
+    for(i=0;i<cdf.length;i++){
+        if(i==0) a = 0;
+        else a = cdf[i-1];
+
+        cdf[i] = a + rVal[i];
+    }
+
+    callback(categoryVal,rVal,cdf);
+}
+
